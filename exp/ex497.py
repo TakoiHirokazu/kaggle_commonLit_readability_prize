aedca6efd2ce16f9185ad6a8d93797cc60630a8d
TakoiHirokazu <takoihirokazu@gmail.com>
2021-08-13 11:17:11 +0900

    exp update

--- exp/ex497.py
+++ exp/ex497.py
@@ -3,17 +3,13 @@
 # ========================================
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import StratifiedKFold, KFold,GroupKFold
 from sklearn.metrics import mean_squared_error
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader, Subset
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from torch.utils.data import Dataset, DataLoader
 import transformers
-from transformers import RobertaModel
 from transformers import AdamW, get_linear_schedule_with_warmup
-from transformers import RobertaForSequenceClassification
-from transformers import RobertaTokenizer,BertTokenizer
+from transformers import BertTokenizer
 import logging
 import sys
 from contextlib import contextmanager
@@ -31,8 +27,9 @@ FOLD_PATH = "../data/fe001_train_folds.csv"
 if not os.path.exists(f"../output/ex/ex{ex}"):
     os.makedirs(f"../output/ex/ex{ex}")
     os.makedirs(f"../output/ex/ex{ex}/ex{ex}_model")
-    
+
 MODEL_PATH_BASE = f"../output/ex/ex{ex}/ex{ex}_model/ex{ex}"
+OOF_SAVE_PATH = f"../output/ex/ex{ex}/ex{ex}_oof.npy"
 LOGGER_PATH = f"../output/ex/ex{ex}/ex{ex}.txt"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -41,19 +38,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Settings
 # ===============
 SEED = 0
-N_SPLITS = 5
-SHUFFLE = True
 num_workers = 4
-
 BATCH_SIZE = 16
 n_epochs = 5
 es_patience = 10
-
 max_len = 256
 weight_decay = 0.1
 lr = 5e-5
 num_warmup_steps_rate = 0.1
-
 eval_steps = 20
 
 BERT_MODEL = 'bert-base-uncased'
@@ -77,9 +69,9 @@ class CommonLitDataset(Dataset):
     def __getitem__(self, item):
         text = str(self.excerpt[item])
         inputs = self.tokenizer(
-            text, 
-            max_length=self.max_len, 
-            padding="max_length", 
+            text,
+            max_length=self.max_len,
+            padding="max_length",
             truncation=True
         )
         ids = inputs["input_ids"]
@@ -89,39 +81,41 @@ class CommonLitDataset(Dataset):
             return {
                 "input_ids": torch.tensor(ids, dtype=torch.long),
                 "attention_mask": torch.tensor(mask, dtype=torch.long),
-                "token_type_ids" : torch.tensor(token_type_ids, dtype=torch.long),
-                "target" : torch.tensor(self.target[item], dtype=torch.float32)
+                "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+                "target": torch.tensor(self.target[item], dtype=torch.float32)
             }
         else:
             return {
                 "input_ids": torch.tensor(ids, dtype=torch.long),
                 "attention_mask": torch.tensor(mask, dtype=torch.long),
-                "token_type_ids" : torch.tensor(token_type_ids, dtype=torch.long)
+                "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long)
             }
+
 
 class bert_base_model(nn.Module):
     def __init__(self):
         super(bert_base_model, self).__init__()
         self.bert = transformers.BertModel.from_pretrained(BERT_MODEL,
-                                                          hidden_dropout_prob = 0,
-                                                          attention_probs_dropout_prob = 0)
-        #self.bert = transformers.BertForSequenceClassification.from_pretrained(BERT_MODEL,num_labels=1)
+                                                           hidden_dropout_prob=0,
+                                                           attention_probs_dropout_prob=0)
+        # self.bert = transformers.BertForSequenceClassification.from_pretrained(BERT_MODEL,num_labels=1)
         self.ln = nn.LayerNorm(768)
         self.out = nn.Linear(768, 1)
-    
+
     def forward(self, ids, mask, token_type_ids):
         # pooler
-        emb , _= self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids, return_dict=False)
+        emb, _ = self.bert(ids, attention_mask=mask,
+                           token_type_ids=token_type_ids, return_dict=False)
         emb = torch.mean(emb, axis=1)
         output = self.ln(emb)
         output = self.out(output)
         return output
 
-    
-    
+
 def calc_loss(y_true, y_pred):
-    return  np.sqrt(mean_squared_error(y_true, y_pred))
-    
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+
 def set_seed(seed: int = 42):
     random.seed(seed)
     np.random.seed(seed)
@@ -129,7 +123,6 @@ def set_seed(seed: int = 42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-
 
 
 def setup_logger(out_file=None, stderr=True, stderr_level=logging.INFO, file_level=logging.DEBUG):
@@ -155,10 +148,10 @@ def setup_logger(out_file=None, stderr=True, stderr_level=logging.INFO, file_lev
 @contextmanager
 def timer(name):
     t0 = time.time()
-    yield 
+    yield
     LOGGER.info(f'[{name}] done in {time.time() - t0:.0f} s')
-    
-    
+
+
 LOGGER = logging.getLogger()
 FORMATTER = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 setup_logger(out_file=LOGGER_PATH)
@@ -179,29 +172,36 @@ fold_array = fold_df["kfold"].values
 with timer("bert uncased"):
     set_seed(SEED)
     oof = np.zeros([len(train)])
-    #kf = KFold(n_splits=N_SPLITS, shuffle=SHUFFLE, random_state=SEED)
     for fold in range(5):
-        x_train, y_train = train.iloc[fold_array != fold], y.iloc[fold_array != fold]
-        x_val, y_val =train.iloc[fold_array == fold], y.iloc[fold_array == fold]
-        
+        x_train, y_train = train.iloc[fold_array !=
+                                      fold], y.iloc[fold_array != fold]
+        x_val, y_val = train.iloc[fold_array ==
+                                  fold], y.iloc[fold_array == fold]
+
         # dataset
-        train_ = CommonLitDataset(x_train["excerpt"].values, tokenizer, max_len, y_train.values.reshape(-1,1))
-        val_ = CommonLitDataset(x_val["excerpt"].values, tokenizer, max_len, y_val.values.reshape(-1,1))
-        
+        train_ = CommonLitDataset(
+            x_train["excerpt"].values, tokenizer, max_len, y_train.values.reshape(-1, 1))
+        val_ = CommonLitDataset(
+            x_val["excerpt"].values, tokenizer, max_len, y_val.values.reshape(-1, 1))
+
         # loader
-        train_loader = DataLoader(dataset=train_, batch_size=BATCH_SIZE, shuffle = True , num_workers=4)
-        val_loader = DataLoader(dataset=val_, batch_size=BATCH_SIZE, shuffle = False , num_workers=4)
-        
+        train_loader = DataLoader(
+            dataset=train_, batch_size=BATCH_SIZE, shuffle=True, num_workers=num_workers)
+        val_loader = DataLoader(
+            dataset=val_, batch_size=BATCH_SIZE, shuffle=False, num_workers=num_workers)
+
         # model
         model = bert_base_model()
         model = model.to(device)
-        
+
         # optimizer, scheduler
         param_optimizer = list(model.named_parameters())
         no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
         optimizer_grouped_parameters = [
-            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
-            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            {'params': [p for n, p in param_optimizer if not any(
+                nd in n for nd in no_decay)], 'weight_decay': weight_decay},
+            {'params': [p for n, p in param_optimizer if any(
+                nd in n for nd in no_decay)], 'weight_decay': 0.0}
         ]
         optimizer = AdamW(optimizer_grouped_parameters,
                           lr=lr,
@@ -209,49 +209,50 @@ with timer("bert uncased"):
                           weight_decay=weight_decay,
                           )
         num_train_optimization_steps = int(len(train_loader) * n_epochs)
-        num_warmup_steps = int(num_train_optimization_steps * num_warmup_steps_rate)
+        num_warmup_steps = int(
+            num_train_optimization_steps * num_warmup_steps_rate)
         scheduler = get_linear_schedule_with_warmup(optimizer,
                                                     num_warmup_steps=num_warmup_steps,
                                                     num_training_steps=num_train_optimization_steps)
-        
+
         criterion = nn.MSELoss()
         best_val = None
         patience = es_patience
         for epoch in tqdm(range(n_epochs)):
             with timer(f"model_fold:{epoch}"):
-                
+
                 # train
-                model.train() 
+                model.train()
                 train_losses_batch = []
-                
+
                 epoch_loss = 0
-                
+
                 for i, d in enumerate(train_loader):
-                    
+
                     input_ids = d['input_ids']
                     mask = d['attention_mask']
                     token_type_ids = d["token_type_ids"]
                     target = d["target"]
-                    
+
                     input_ids = input_ids.to(device)
                     mask = mask.to(device)
                     token_type_ids = token_type_ids.to(device)
                     target = target.to(device)
                     optimizer.zero_grad()
-                    output = model(input_ids, mask, token_type_ids )
+                    output = model(input_ids, mask, token_type_ids)
                     loss = criterion(output, target)
                     loss.backward()
                     torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                     optimizer.step()
                     scheduler.step()
                     train_losses_batch.append(loss.item())
-                    
+
                     if i % eval_steps == 0:
-                            # val
+                        # val
                         val_losses_batch = []
                         model.eval()  # switch model to the evaluation mode
-                        val_preds = np.ndarray((0,1))
-                        with torch.no_grad():  
+                        val_preds = np.ndarray((0, 1))
+                        with torch.no_grad():
                             # Predicting on validation set
                             for d in val_loader:
                                 # =========================
@@ -266,17 +267,17 @@ with timer("bert uncased"):
                                 mask = mask.to(device)
                                 token_type_ids = token_type_ids.to(device)
                                 target = target.to(device)
-                                output = model(input_ids, mask,token_type_ids )
+                                output = model(input_ids, mask, token_type_ids)
 
                                 loss = criterion(output, target)
-                                val_preds = np.concatenate([val_preds, output.detach().cpu().numpy()], axis=0)
+                                val_preds = np.concatenate(
+                                    [val_preds, output.detach().cpu().numpy()], axis=0)
                                 val_losses_batch.append(loss.item())
-
-
 
                         val_loss = np.mean(val_losses_batch)
                         val_rmse = calc_loss(y_val, val_preds)
-                        LOGGER.info(f'{fold},{epoch}:{i},val_loss:{val_loss},val_rmse:{val_rmse}')
+                        LOGGER.info(
+                            f'{fold},{epoch}:{i},val_loss:{val_loss},val_rmse:{val_rmse}')
                         # ===================
                         # early stop
                         # ===================
@@ -285,7 +286,9 @@ with timer("bert uncased"):
                             best_val = val_loss
                             best_rmse = val_rmse
                             oof[fold_array == fold] = val_preds.reshape(-1)
-                            torch.save(model.state_dict(), MODEL_PATH_BASE + f"_{fold}.pth")  # Saving the model
+                            # Saving the model
+                            torch.save(model.state_dict(),
+                                       MODEL_PATH_BASE + f"_{fold}.pth")
                             continue
 
                         if val_loss <= best_val:
@@ -293,16 +296,18 @@ with timer("bert uncased"):
                             best_rmse = val_rmse
                             oof[fold_array == fold] = val_preds.reshape(-1)
                             patience = es_patience
-                            torch.save(model.state_dict(), MODEL_PATH_BASE + f"_{fold}.pth") # Saving current best model
-                        #else:
+                            # Saving current best model
+                            torch.save(model.state_dict(),
+                                       MODEL_PATH_BASE + f"_{fold}.pth")
+                        # else:
                         #    patience -= 1
                         #    if patience == 0:
                         #        LOGGER.info(f'Early stopping. Best Val : {best_val} Best Rmse : {best_rmse}')
                         #        break
-                        model.train() 
+                        model.train()
 
                 train_loss = np.mean(train_losses_batch)
-                
+
 val_rmse = calc_loss(y, oof)
 LOGGER.info(f'oof_score:{val_rmse}')
-np.save(f"../output/ex/ex{ex}/ex{ex}_oof.npy",oof)
+np.save(OOF_SAVE_PATH, oof)
--- exp/ex507.py
+++ exp/ex507.py
@@ -3,17 +3,13 @@
 # ========================================
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import StratifiedKFold, KFold,GroupKFold
 from sklearn.metrics import mean_squared_error
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader, Subset
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-import transformers
-from transformers import RobertaModel,BartModel
+from torch.utils.data import Dataset, DataLoader
+from transformers import BartModel
 from transformers import AdamW, get_linear_schedule_with_warmup
-from transformers import RobertaForSequenceClassification
-from transformers import RobertaTokenizer,BartTokenizer
+from transformers import RobertaTokenizer
 import logging
 import sys
 from contextlib import contextmanager
@@ -32,8 +28,9 @@ FOLD_PATH = "../data/fe001_train_folds.csv"
 if not os.path.exists(f"../output/ex/ex{ex}"):
     os.makedirs(f"../output/ex/ex{ex}")
     os.makedirs(f"../output/ex/ex{ex}/ex{ex}_model")
-    
+
 MODEL_PATH_BASE = f"../output/ex/ex{ex}/ex{ex}_model/ex{ex}"
+OOF_SAVE_PATH = f"../output/ex/ex{ex}/ex{ex}_oof.npy"
 LOGGER_PATH = f"../output/ex/ex{ex}/ex{ex}.txt"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -42,19 +39,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Settings
 # ===============
 SEED = 0
-N_SPLITS = 5
-SHUFFLE = True
 num_workers = 4
-
 BATCH_SIZE = 16
 n_epochs = 5
 es_patience = 10
-
 max_len = 256
 weight_decay = 0.1
 lr = 3e-5
 num_warmup_steps_rate = 0.1
-
 eval_steps = 20
 
 MODEL_PATH = 'sshleifer/distilbart-cnn-12-6'
@@ -78,9 +70,9 @@ class CommonLitDataset(Dataset):
     def __getitem__(self, item):
         text = str(self.excerpt[item])
         inputs = self.tokenizer(
-            text, 
-            max_length=self.max_len, 
-            padding="max_length", 
+            text,
+            max_length=self.max_len,
+            padding="max_length",
             truncation=True,
             return_attention_mask=True,
             return_token_type_ids=True
@@ -92,42 +84,44 @@ class CommonLitDataset(Dataset):
             return {
                 "input_ids": torch.tensor(ids, dtype=torch.long),
                 "attention_mask": torch.tensor(mask, dtype=torch.long),
-                "token_type_ids" : torch.tensor(token_type_ids, dtype=torch.long),
-                "target" : torch.tensor(self.target[item], dtype=torch.float32)
+                "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+                "target": torch.tensor(self.target[item], dtype=torch.float32)
             }
         else:
             return {
                 "input_ids": torch.tensor(ids, dtype=torch.long),
                 "attention_mask": torch.tensor(mask, dtype=torch.long),
-                "token_type_ids" : torch.tensor(token_type_ids, dtype=torch.long)
+                "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long)
             }
+
 
 class distil_bart_model(nn.Module):
     def __init__(self):
         super(distil_bart_model, self).__init__()
         self.bart = BartModel.from_pretrained(
-            MODEL_PATH, 
+            MODEL_PATH,
             activation_dropout=0.0, attention_dropout=0.0,
-            classif_dropout = 0, classifier_dropout = 0
+            classif_dropout=0, classifier_dropout=0
         )
-        
-        #self.dropout = nn.Dropout(p=0.2)
+
+        # self.dropout = nn.Dropout(p=0.2)
         self.ln = nn.LayerNorm(1024)
         self.out = nn.Linear(1024, 1)
-    
+
     def forward(self, ids, mask):
         # pooler
         emb = self.bart(ids, attention_mask=mask)['last_hidden_state']
-        emb = torch.mean(emb,axis=1)
+        emb = torch.mean(emb, axis=1)
         output = self.ln(emb)
-        #output = self.dropout(output)
+        # output = self.dropout(output)
         output = self.out(output)
-        return output,emb
-    
-    
+        return output, emb
+
+
 def calc_loss(y_true, y_pred):
-    return  np.sqrt(mean_squared_error(y_true, y_pred))
-    
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+
 def set_seed(seed: int = 42):
     random.seed(seed)
     np.random.seed(seed)
@@ -135,7 +129,6 @@ def set_seed(seed: int = 42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-
 
 
 def setup_logger(out_file=None, stderr=True, stderr_level=logging.INFO, file_level=logging.DEBUG):
@@ -161,10 +154,10 @@ def setup_logger(out_file=None, stderr=True, stderr_level=logging.INFO, file_lev
 @contextmanager
 def timer(name):
     t0 = time.time()
-    yield 
+    yield
     LOGGER.info(f'[{name}] done in {time.time() - t0:.0f} s')
-    
-    
+
+
 LOGGER = logging.getLogger()
 FORMATTER = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 setup_logger(out_file=LOGGER_PATH)
@@ -184,29 +177,36 @@ fold_array = fold_df["kfold"].values
 with timer("distil_bart"):
     set_seed(SEED)
     oof = np.zeros([len(train)])
-    #kf = KFold(n_splits=N_SPLITS, shuffle=SHUFFLE, random_state=SEED)
     for fold in range(5):
-        x_train, y_train = train.iloc[fold_array != fold], y.iloc[fold_array != fold]
-        x_val, y_val =train.iloc[fold_array == fold], y.iloc[fold_array == fold]
-        
+        x_train, y_train = train.iloc[fold_array !=
+                                      fold], y.iloc[fold_array != fold]
+        x_val, y_val = train.iloc[fold_array ==
+                                  fold], y.iloc[fold_array == fold]
+
         # dataset
-        train_ = CommonLitDataset(x_train["excerpt"].values, tokenizer, max_len, y_train.values.reshape(-1,1))
-        val_ = CommonLitDataset(x_val["excerpt"].values, tokenizer, max_len, y_val.values.reshape(-1,1))
-        
+        train_ = CommonLitDataset(
+            x_train["excerpt"].values, tokenizer, max_len, y_train.values.reshape(-1, 1))
+        val_ = CommonLitDataset(
+            x_val["excerpt"].values, tokenizer, max_len, y_val.values.reshape(-1, 1))
+
         # loader
-        train_loader = DataLoader(dataset=train_, batch_size=BATCH_SIZE, shuffle = True , num_workers=4)
-        val_loader = DataLoader(dataset=val_, batch_size=BATCH_SIZE, shuffle = False , num_workers=4)
-        
+        train_loader = DataLoader(
+            dataset=train_, batch_size=BATCH_SIZE, shuffle=True, num_workers=num_workers)
+        val_loader = DataLoader(
+            dataset=val_, batch_size=BATCH_SIZE, shuffle=False, num_workers=num_workers)
+
         # model
         model = distil_bart_model()
         model = model.to(device)
-        
+
         # optimizer, scheduler
         param_optimizer = list(model.named_parameters())
         no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
         optimizer_grouped_parameters = [
-            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
-            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            {'params': [p for n, p in param_optimizer if not any(
+                nd in n for nd in no_decay)], 'weight_decay': weight_decay},
+            {'params': [p for n, p in param_optimizer if any(
+                nd in n for nd in no_decay)], 'weight_decay': 0.0}
         ]
         optimizer = AdamW(optimizer_grouped_parameters,
                           lr=lr,
@@ -214,49 +214,50 @@ with timer("distil_bart"):
                           weight_decay=weight_decay,
                           )
         num_train_optimization_steps = int(len(train_loader) * n_epochs)
-        num_warmup_steps = int(num_train_optimization_steps * num_warmup_steps_rate)
+        num_warmup_steps = int(
+            num_train_optimization_steps * num_warmup_steps_rate)
         scheduler = get_linear_schedule_with_warmup(optimizer,
                                                     num_warmup_steps=num_warmup_steps,
                                                     num_training_steps=num_train_optimization_steps)
-        
+
         criterion = nn.MSELoss()
         best_val = None
         patience = es_patience
         for epoch in tqdm(range(n_epochs)):
             with timer(f"model_fold:{epoch}"):
-                
+
                 # train
-                model.train() 
+                model.train()
                 train_losses_batch = []
-                
+
                 epoch_loss = 0
-                
+
                 for i, d in enumerate(train_loader):
-                    
+
                     input_ids = d['input_ids']
                     mask = d['attention_mask']
                     token_type_ids = d["token_type_ids"]
                     target = d["target"]
-                    
+
                     input_ids = input_ids.to(device)
                     mask = mask.to(device)
                     token_type_ids = token_type_ids.to(device)
                     target = target.to(device)
                     optimizer.zero_grad()
-                    output,_ = model(input_ids, mask)
+                    output, _ = model(input_ids, mask)
                     loss = criterion(output, target)
                     loss.backward()
                     torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                     optimizer.step()
                     scheduler.step()
                     train_losses_batch.append(loss.item())
-                    
+
                     if i % eval_steps == 0:
-                            # val
+                        # val
                         val_losses_batch = []
                         model.eval()  # switch model to the evaluation mode
-                        val_preds = np.ndarray((0,1))
-                        with torch.no_grad():  
+                        val_preds = np.ndarray((0, 1))
+                        with torch.no_grad():
                             # Predicting on validation set
                             for d in val_loader:
                                 # =========================
@@ -271,26 +272,28 @@ with timer("distil_bart"):
                                 mask = mask.to(device)
                                 token_type_ids = token_type_ids.to(device)
                                 target = target.to(device)
-                                output,_ = model(input_ids, mask)
+                                output, _ = model(input_ids, mask)
 
                                 loss = criterion(output, target)
-                                val_preds = np.concatenate([val_preds, output.detach().cpu().numpy()], axis=0)
+                                val_preds = np.concatenate(
+                                    [val_preds, output.detach().cpu().numpy()], axis=0)
                                 val_losses_batch.append(loss.item())
-
-
 
                         val_loss = np.mean(val_losses_batch)
                         val_rmse = calc_loss(y_val, val_preds)
-                        LOGGER.info(f'{fold},{epoch}:{i},val_loss:{val_loss},val_rmse:{val_rmse}')
+                        LOGGER.info(
+                            f'{fold},{epoch}:{i},val_loss:{val_loss},val_rmse:{val_rmse}')
                         # ===================
                         # early stop
                         # ===================
 
                         if not best_val:
-                            best_val = val_loss 
+                            best_val = val_loss
                             best_rmse = val_rmse
                             oof[fold_array == fold] = val_preds.reshape(-1)
-                            torch.save(model.state_dict(), MODEL_PATH_BASE + f"_{fold}.pth")  # Saving the model
+                            # Saving the model
+                            torch.save(model.state_dict(),
+                                       MODEL_PATH_BASE + f"_{fold}.pth")
                             continue
 
                         if val_loss <= best_val:
@@ -298,16 +301,18 @@ with timer("distil_bart"):
                             best_rmse = val_rmse
                             oof[fold_array == fold] = val_preds.reshape(-1)
                             patience = es_patience
-                            torch.save(model.state_dict(), MODEL_PATH_BASE + f"_{fold}.pth") # Saving current best model
-                        #else:
+                            # Saving current best model
+                            torch.save(model.state_dict(),
+                                       MODEL_PATH_BASE + f"_{fold}.pth")
+                        # else:
                         #    patience -= 1
                         #    if patience == 0:
                         #        LOGGER.info(f'Early stopping. Best Val : {best_val} Best Rmse : {best_rmse}')
                         #        break
-                        model.train() 
+                        model.train()
 
                 train_loss = np.mean(train_losses_batch)
-                
+
 val_rmse = calc_loss(y, oof)
 LOGGER.info(f'oof_score:{val_rmse}')
-np.save(f"../output/ex/ex{ex}/ex{ex}_oof.npy",oof)
+np.save(OOF_SAVE_PATH, oof)
